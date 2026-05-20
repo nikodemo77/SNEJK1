49219daeb0af02083f9e8937d91323d54e431b06
Nikodem Szydlowski <nikodemsz2010@gmail.com>
2026-05-20 09:33:02 +0200

    UPDATE

--- SNEJK1/Form2.cs
+++ SNEJK1/Form2.cs
@@ -21,6 +21,7 @@ namespace SNEJK1
         int speed = 20;
 
         Random random = new Random();
+        List<PictureBox> snake = new List<PictureBox>();
     
 
         public Form2()
@@ -38,6 +39,7 @@ namespace SNEJK1
             head.Top = 100;
 
             this.Controls.Add(head);
+            snake.Add(head);
 
             food.BackColor = Color.Red;
 
@@ -52,46 +54,66 @@ namespace SNEJK1
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (direction == "right")
+            for (int i = snake.Count - 1; i > 0; i--)
             {
-                head.Left += speed;
+                for (int j = snake.Count - 1; i > 0; i--)
+                {
+                    snake[i].Left = snake[i - 1].Left;
+                    snake[i].Top = snake[i - 1].Top;
+                }
             }
 
-            if (direction == "left")
-            {
-                head.Left -= speed;
+                if (direction == "right")
+                {
+                    head.Left += speed;
+                }
+
+                if (direction == "left")
+                {
+                    head.Left -= speed;
+                }
+
+                if (direction == "up")
+                {
+                    head.Top -= speed;
+                }
+
+                if (direction == "down")
+                {
+                    head.Top += speed;
+                }
+
+                if (head.Bounds.IntersectsWith(food.Bounds))
+                {
+                    food.Left = random.Next(0, 700);
+                    food.Top = random.Next(0, 400);
+
+                    PictureBox body = new PictureBox();
+                    
+                    body.BackColor = Color.Green;
+
+                    body.Width = 20;
+                    body.Height = 20;
+                    
+                    this.Controls.Add(body);
+
+                    snake.Add(body);
+                }
+
+                if (head.Left < 0 ||
+                    head.Top < 0 ||
+                    head.Right > this.ClientSize.Width ||
+                    head.Bottom > this.ClientSize.Height)
+                {
+                    timer1.Stop();
+
+                    MessageBox.Show("Przegrałeś");
+                    MessageBox.Show("Przegrałeś");
+                    MessageBox.Show("Przegrałeś");
+                    MessageBox.Show("Wygrałeś!");
+                }
+
             }
-
-            if (direction == "up")
-            {
-                head.Top -= speed;
-            }
-
-            if (direction == "down")
-            {
-                head.Top += speed;
-            }
-
-            if (head.Bounds.IntersectsWith(food.Bounds))
-            {
-                food.Left = random.Next(0, 700);
-                food.Top = random.Next(0, 400);
-            }
-
-            if (head.Left < 0 ||
-                head.Top < 0 ||
-                head.Right > this.ClientSize.Width ||
-                head.Bottom > this.ClientSize.Height)
-            {
-                timer1.Stop();
-
-                MessageBox.Show("Przegrałeś");
-                MessageBox.Show("Przegrałeś");
-                MessageBox.Show("Przegrałeś");
-                MessageBox.Show("Wygrałeś!");
-            }
-
-        }
 
         private void Form2_KeyDown(object sender, KeyEventArgs e)
         {
